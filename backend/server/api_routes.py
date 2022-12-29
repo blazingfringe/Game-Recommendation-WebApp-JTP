f36d25058f3d55276b7fd0e28fe13de4ba8e6ed5
blazingfringe <ayushkr101@gmail.com>
2022-12-29 21:52:52 +0530

    Optimize Data Retreival from Repository

--- backend/server/api_routes.py
+++ backend/server/api_routes.py
@@ -3,7 +3,6 @@ from flask import jsonify, request
 from flask_cors import cross_origin
 from server.repository import get_game_list, getEachRecommendation, ready_response
 from server.recommendations import give_recommendations
-from server.repository import connection
 import sys
 
 
@@ -23,7 +22,7 @@ def get_recommendation():
     length = len(data)
     print(length, file=sys.stdout)
     for i in range(length):
-        gg = give_recommendations(str(data[i]), True)
+        gg = give_recommendations(str(data[i]))
         for gam in gg['Games']:
             js_data = getEachRecommendation(gam)
     return jsonify(js_data)
--- backend/server/recommendations.py
+++ backend/server/recommendations.py
@@ -7,24 +7,17 @@ from server.repository import get_all_games, get_embed_data, get_pca_data
 
 df = get_all_games()
 pca_data = get_pca_data()
-embd = get_embed_data()
+word_vectors = get_embed_data()
 
 
-X = np.array(embd)
-cos_sim_data = pd.DataFrame(cosine_similarity(X))
+cos_sim_values = pd.DataFrame(cosine_similarity(word_vectors))
 indices = pd.Series(pca_data.index, index=df['title'])
 
 
-def give_recommendations(name, print_recommendation=False):
+def give_recommendations(name):
     index = indices[name]
-    index_recomm = cos_sim_data.loc[index].sort_values(
+    index_recomm = cos_sim_values.loc[index].sort_values(
         ascending=False).index.tolist()[1:6]
     games_rec = df['title'].loc[index_recomm].values
     result = {'Games': games_rec, 'Index': index_recomm}
-    if print_recommendation == True:
-        print('The Played Game is this one: %s \n' %
-              (df['title'].loc[index]))
-        k = 1
-        for game in games_rec:
-            print('The number %i recommended game is this one: %s \n' % (k, game))
     return result
